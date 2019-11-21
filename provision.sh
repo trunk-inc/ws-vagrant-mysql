f3bd1bf7d856c9fd6c5f6716ceb53f4fe2cd0e26
Heiya <morooka.akira.inon29@gmail.com>
2019-11-21 13:17:05 +0900

    Add Vagrantfile.

---
+++ provision.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -lu
+sudo apt-get update
+
+# MySQL
+export DEBIAN_FRONTEND="noninteractive"
+
+sudo debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password password \"''\""
+sudo debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password_again password \"''\""
+
+sudo apt-get install -y mysql-server-5.6 
