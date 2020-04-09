eba99287af6b8d78af00909619b2f335d0994b73
Heiya <morooka.akira.inon29@gmail.com>
2020-04-09 17:33:55 +0900

    fix typo table_name;

--- sql/section-3/tapioca_shop.sql
+++ sql/section-3/tapioca_shop.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS tapioca_drink (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS tapioca_salse (
+CREATE TABLE IF NOT EXISTS tapioca_sales (
     id INT, 
     drink_id VARCHAR(100),
     purchase_date DATE,
@@ -18,18 +18,18 @@ INSERT IGNORE INTO tapioca_drink VALUES ('d-2', '抹茶ミルク', 500);
 INSERT IGNORE INTO tapioca_drink VALUES ('d-3', '黒糖ミルク', 450);
 INSERT IGNORE INTO tapioca_drink VALUES ('d-4', 'いちごミルク', 530);
 
-INSERT IGNORE INTO tapioca_salse VALUES (1, 'd-1', '2020-02-02');
-INSERT IGNORE INTO tapioca_salse VALUES (2, 'd-1', '2020-02-03');
-INSERT IGNORE INTO tapioca_salse VALUES (3, 'd-2', '2020-02-03');
-INSERT IGNORE INTO tapioca_salse VALUES (4, 'd-4', '2020-02-13');
-INSERT IGNORE INTO tapioca_salse VALUES (5, 'd-1', '2020-02-02');
-INSERT IGNORE INTO tapioca_salse VALUES (6, 'd-1', '2020-02-03');
-INSERT IGNORE INTO tapioca_salse VALUES (7, 'd-4', '2020-02-03');
-INSERT IGNORE INTO tapioca_salse VALUES (8, 'd-4', '2020-02-04');
-INSERT IGNORE INTO tapioca_salse VALUES (9, 'd-4', '2020-02-05');
-INSERT IGNORE INTO tapioca_salse VALUES (10, 'd-3', '2020-02-10');
-INSERT IGNORE INTO tapioca_salse VALUES (11, 'd-2', '2020-02-13');
-INSERT IGNORE INTO tapioca_salse VALUES (12, 'd-3', '2020-02-04');
-INSERT IGNORE INTO tapioca_salse VALUES (13, 'd-2', '2020-02-05');
-INSERT IGNORE INTO tapioca_salse VALUES (14, 'd-4', '2020-02-10');
-INSERT IGNORE INTO tapioca_salse VALUES (15, 'd-3', '2020-02-10');
+INSERT IGNORE INTO tapioca_sales VALUES (1, 'd-1', '2020-02-02');
+INSERT IGNORE INTO tapioca_sales VALUES (2, 'd-1', '2020-02-03');
+INSERT IGNORE INTO tapioca_sales VALUES (3, 'd-2', '2020-02-03');
+INSERT IGNORE INTO tapioca_sales VALUES (4, 'd-4', '2020-02-13');
+INSERT IGNORE INTO tapioca_sales VALUES (5, 'd-1', '2020-02-02');
+INSERT IGNORE INTO tapioca_sales VALUES (6, 'd-1', '2020-02-03');
+INSERT IGNORE INTO tapioca_sales VALUES (7, 'd-4', '2020-02-03');
+INSERT IGNORE INTO tapioca_sales VALUES (8, 'd-4', '2020-02-04');
+INSERT IGNORE INTO tapioca_sales VALUES (9, 'd-4', '2020-02-05');
+INSERT IGNORE INTO tapioca_sales VALUES (10, 'd-3', '2020-02-10');
+INSERT IGNORE INTO tapioca_sales VALUES (11, 'd-2', '2020-02-13');
+INSERT IGNORE INTO tapioca_sales VALUES (12, 'd-3', '2020-02-04');
+INSERT IGNORE INTO tapioca_sales VALUES (13, 'd-2', '2020-02-05');
+INSERT IGNORE INTO tapioca_sales VALUES (14, 'd-4', '2020-02-10');
+INSERT IGNORE INTO tapioca_sales VALUES (15, 'd-3', '2020-02-10');
